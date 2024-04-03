5597580f56d50367ca807180c3fdce87b1587d2d
Thomas Gessey-Jones <tg400@cam.ac.uk>
2024-04-03 15:03:13 +0100

    Add timings

--- exercises/My_Solutions/Parralel_Pi/pi.cc
+++ exercises/My_Solutions/Parralel_Pi/pi.cc
@@ -5,6 +5,7 @@ using namespace std;
 
 // Constants
 int ACCURACY_N = 840;  // Number of terms in the series e.g., the accuracy of the calculation
+int NUM_TIMES_TO_COMPUTE_PI = 10000;  // Number of times to compute pi
 
 double partial_sum(int start, int end, int total_n) {
     // Calculate the partial sum of 1/(1 + ((i-0.5)/total_n)^2) from i=start to i=end
@@ -31,6 +32,7 @@ int main() {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    cout.precision(10);  // Will want pi to many decimal places
     cout << "Hello from process " << rank << " of " << size << endl;
 
     // Calculate the partial sum attributed to this process
@@ -43,30 +45,43 @@ int main() {
     int sum_start = rank * terms_per_rank + 1;
     int sum_end = (rank + 1) * terms_per_rank;
 
-    double part_sum = partial_sum(sum_start, sum_end, ACCURACY_N);
-    cout << "Process " << rank << " partial sum " << part_sum << endl;  // Output
+    // Start timing
+    MPI_Barrier(MPI_COMM_WORLD); // Line up at the start line
+    tstart = MPI_Wtime(); // Fire the gun and start the clock
 
-    //Calculate the final sum
-    double total_sum;
-    double received_sum;
-    MPI_Status status;
-    if (rank == 0) {  // Rank 0 is to do the final sum
-        total_sum = part_sum;
-        for (int sender_rank=1; sender_rank<size; sender_rank++) {
-            MPI_Recv(&received_sum, 1, MPI_DOUBLE, sender_rank, 0, MPI_COMM_WORLD, &status);
-            total_sum += received_sum;
+    // To get meaningful results, we will calculate pi NUM_TIMES_TO_COMPUTE_PI times
+    for (int pi_calc_num = 0; pi_calc_num < NUM_TIMES_TO_COMPUTE_PI; pi_calc_num++) {
+        // Partial sum for this process
+        double part_sum = partial_sum(sum_start, sum_end, ACCURACY_N);
+        //cout << "Process " << rank << " partial sum " << part_sum << endl;  // Output
+
+        //Calculate the final sum
+        double total_sum;
+        double received_sum;
+        MPI_Status status;
+        if (rank == 0) {  // Rank 0 is to do the final sum
+            total_sum = part_sum;
+            for (int sender_rank = 1; sender_rank < size; sender_rank++) {
+                MPI_Recv(&received_sum, 1, MPI_DOUBLE, sender_rank, 0, MPI_COMM_WORLD, &status);
+                total_sum += received_sum;
+            }
+
+            // Calculate pi, printing to 10 decimal places
+            double pi = (4.0 / double(ACCURACY_N)) * total_sum;
+
+        } else {
+            MPI_Send(&part_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
     }
-    else {
-        MPI_Send(&part_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-        MPI_Finalize();
-        return 0;
-    }
 
-    // Calculate pi, printing to 10 decimal places
-    double pi = (4.0 / double(ACCURACY_N)) * total_sum;
-    cout.precision(10);
-    cout << "Pi is approximately " << pi << endl;
+    // Output the final pi
+    if (rank == 0) {cout << "Final pi: " << pi << endl;}
+
+    // Finish timing
+    MPI_Barrier(MPI_COMM_WORLD); // Wait for everyone to finish
+    tstop = MPI_WTime(); // Stop the clock
+    if (rank == 0) {cout << "Time taken: " << tstop} - tstart << " seconds" << endl;}
+
     MPI_Finalize();
     return 0;
 }
