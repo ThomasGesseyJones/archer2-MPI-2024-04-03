3399259b4756aadc1596c5ed8218c93736c175a2
Thomas Gessey-Jones <tg400@cam.ac.uk>
2024-04-03 19:50:03 +0100

    Investigate scaling

--- exercises/My_Solutions/Ping_Pong/ping_pong.cc
+++ exercises/My_Solutions/Ping_Pong/ping_pong.cc
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-// Constants
-int NUM_TIMES_TO_EXCHANGE = 10000;  // Number of times to communicate back and forth
-
 
 int main() {
     // Aim is to calculate pi using a parallel algorithm
@@ -21,30 +18,38 @@ int main() {
     MPI_Comm_size(comm, &size);
     cout << "Hello from process " << rank << " of " << size << endl;
 
-    // Start timing
-    MPI_Barrier(comm); // Line up at the start line
-    double tstart = MPI_Wtime(); // Fire the gun and start the clock
+    // List of analyses to perform
+    int num_analyses = 10;
+    int sizes[] = {300, 1000, 3000, 10000, 30000, 100000, 300000, 1000000, 3000000, 10000000};
+    int iterations[] = {10000, 3000, 1000, 300, 100, 100, 100, 100, 100, 100};
 
-    // Pass array back and forth
-    int array_size = 100;
-    int array[array_size];
-    for (int idx=0; idx<array_size; idx++) {array[idx] = idx;}
+    for (int analysis_id=0; analysis_id<num_analyses; analysis_id++) {
+        // Start timing
+        MPI_Barrier(comm); // Line up at the start line
+        double tstart = MPI_Wtime(); // Fire the gun and start the clock
 
-    MPI_Status status;
-    for (int exchange_idx=0; exchange_idx<NUM_TIMES_TO_EXCHANGE; exchange_idx++){
-        if (rank == 0){
-            MPI_Ssend(array, array_size, MPI_INT, 1, 0, comm);
-            MPI_Recv(array, array_size, MPI_INT, 1, 0, comm, &status);
-        } else if (rank == 1){
-            MPI_Recv(array, array_size, MPI_INT, 0, 0, comm, &status);
-            MPI_Ssend(array, array_size, MPI_INT, 0, 0, comm);
+        // Pass array back and forth
+        int num_times_to_exchange = iterations[analysis_id];
+        int array_size = sizes[analysis_id];
+        int array[array_size];
+        for (int idx = 0; idx < array_size; idx++) { array[idx] = idx; }
+
+        MPI_Status status;
+        for (int exchange_idx = 0; exchange_idx < num_times_to_exchange; exchange_idx++) {
+            if (rank == 0) {
+                MPI_Ssend(array, array_size, MPI_INT, 1, 0, comm);
+                MPI_Recv(array, array_size, MPI_INT, 1, 0, comm, &status);
+            } else if (rank == 1) {
+                MPI_Recv(array, array_size, MPI_INT, 0, 0, comm, &status);
+                MPI_Ssend(array, array_size, MPI_INT, 0, 0, comm);
+            }
         }
-    }
 
-    // Finish timing
-    MPI_Barrier(comm); // Wait for everyone to finish
-    double tstop = MPI_Wtime(); // Stop the clock
-    if (rank == 0) {cout << "Time taken: " << tstop - tstart << " seconds" << endl;}
+        // Finish timing
+        MPI_Barrier(comm); // Wait for everyone to finish
+        double tstop = MPI_Wtime(); // Stop the clock
+        if (rank == 0) { cout << "Time taken: " << tstop - tstart << " seconds for analysis " << analysis_id << endl; }
+    }
 
     MPI_Finalize();
     return 0;
